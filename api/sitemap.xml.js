72a7009141d5f0185a118e7187c81149d4244126
zedaye9664-art <zedaye9664@gmail.com>
2026-09-16 15:16:04 +0500

    Update sitemap.xml.js

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -8,27 +8,33 @@ export default async function handler(req, res) {
       return d.documents || [];
     };
 
+    const getField = (doc, fieldName) => {
+      const f = doc.fields?.[fieldName];
+      if (!f) return null;
+      return f.stringValue || f.integerValue || null;
+    };
+
     let xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
-    xml += `  <url><loc>https://www.zedlinker.store/</loc><changefreq>daily</changefreq><priority>1.0</priority></url>\n`;
-    xml += `  <url><loc>https://www.zedlinker.store/blog</loc><changefreq>daily</changefreq><priority>0.8</priority></url>\n`;
+    xml += ` <url><loc>https://www.zedlinker.store/</loc><changefreq>daily</changefreq><priority>1.0</priority></url>\n`;
+    xml += ` <url><loc>https://www.zedlinker.store/blog</loc><changefreq>daily</changefreq><priority>0.8</priority></url>\n`;
 
     const blogs = await fetchCol('blogs');
     blogs.forEach(doc => {
-      const name = doc.name.split('/').pop();
-      xml += `  <url><loc>https://www.zedlinker.store/blog/${name}</loc><changefreq>weekly</changefreq><priority>0.8</priority></url>\n`;
+      const slug = getField(doc, 'slug') || getField(doc, 'id') || doc.name.split('/').pop();
+      xml += ` <url><loc>https://www.zedlinker.store/blog/${slug}</loc><changefreq>weekly</changefreq><priority>0.8</priority></url>\n`;
     });
 
     const links = await fetchCol('links');
     links.forEach(doc => {
-      const name = doc.name.split('/').pop();
-      xml += `  <url><loc>https://www.zedlinker.store/link/${name}</loc><changefreq>daily</changefreq><priority>0.9</priority></url>\n`;
+      const slug = getField(doc, 'slug') || getField(doc, 'linkId') || getField(doc, 'id') || doc.name.split('/').pop();
+      xml += ` <url><loc>https://www.zedlinker.store/link/${slug}</loc><changefreq>daily</changefreq><priority>0.9</priority></url>\n`;
     });
 
     xml += `</urlset>`;
-    
+
     res.setHeader('Content-Type', 'text/xml');
     res.status(200).send(xml);
   } catch (e) {
     res.status(500).send("Error: " + e.message);
   }
-      }
+                  }
